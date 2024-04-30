7e833b0cb8da109efad3f26671adf6a5514bfa62
Khludnev V. <vskhludnev@gmail.com>
2024-04-30 14:20:43 +0300

    Create structure in data.py

---
+++ data.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class Database():
+    def __init__():
+        pass
+
+
+
+    def add_user_data():
+        pass
+
+    def get_user_data():
+        pass
+
+
+
+    def add_account_data():
+        pass
+
+    def delete_account_data():
+        pass
+
+    def get_account_data():
+        pass
